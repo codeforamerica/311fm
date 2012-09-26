1559d4a7afb06807689bc73456e1311bac473b78
Mick Thompson <dthompson@gmail.com>
2012-09-26 14:24:43 -0700

    basic browse

--- app/modules/browse.js
+++ app/modules/browse.js
@@ -19,13 +19,18 @@ function(app) {
     },
 
     cleanup: function() {
+      this.serviceRequests.off(null, null, this);
       app.off(null, null, this);
     },
     serialize: function(){
-
+      return {serviceRequests:this.serviceRequests.toJSON()}
     },
     initialize: function(o) {
       this.filters = o.filters;
+      this.serviceRequests  = o.serviceRequests;
+      this.serviceRequests.on("reset", this.render, this);
+      this.serviceRequests.on("change", this.render, this);
+      this.serviceRequests.on("add", this.render, this);
 
       app.on("show_filters", function(){$("#content").addClass("sidebar");}, this);
       app.on("show_nav", function(){$("#content").removeClass("sidebar");}, this);
